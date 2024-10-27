18eba1b87e8627a8c282b962b1a51bd20ec63e0e
Agneya48 <josh.t.hampton@gmail.com>
2024-10-27 16:01:33 -0400

    Initial Commit

---
+++ src/DevTest.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class DevTest {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String input = getNonZeroLenString(in, "Please enter a string for testing");
+        System.out.println("You entered " + input);
+
+    }
+
+    private static String getNonZeroLenString(Scanner pipe, String prompt) {
+        /** @param pipe a Scanner opened to read from System.in
+         * @param prompt prompt for the user
+         * @return a String response that is not zero length
+         */
+        String retString = ""; // Loop runs until string isn't blank
+        do {
+            System.out.print("\n" + prompt + ": ");
+            retString = pipe.nextLine();
+        } while(retString.length() == 0);
+
+        return retString;
+    }
+
+
+}
